9bd07700b430808d362f970884436084dd0ec9fa
Codelover45 <85251886+Codelover45@users.noreply.github.com>
2022-02-26 13:14:07 -0500

    Add files via upload

---
+++ kwitter.login.js
@@ -0,0 +1,11 @@
+
+  function adduser() {
+
+  
+  
+var Username = document.getElementById("user-name").value;
+localStorage.setItem("user name", Username);
+
+ window.location("kwitter_room_html");
+
+  }
